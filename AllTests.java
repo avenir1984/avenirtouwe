bbece4bca626727c727dfec530b879e9741b40de
avenir1984 <60416229+avenir1984@users.noreply.github.com>
2020-07-12 20:07:33 -0400

    Add files via upload

---
+++ AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CryptoJUnitTest.class })
+public class AllTests {
+
+}
